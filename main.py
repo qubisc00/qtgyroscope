4dee9f3891c2beac7da2347941171d3640a2e0fe
qubisc <174634622+qubisc00@users.noreply.github.com>
2025-03-07 23:58:41 +0100

    feat: export to own module

--- main.py
+++ main.py
@@ -1,63 +1,11 @@
-# Copyright (C) 2022 The Qt Company Ltd.
-# SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
-from __future__ import annotations
-
-"""PySide6 QtDataVisualization example"""
-
 import sys
 from PySide6.QtCore import Qt
-from PySide6.QtGui import QGuiApplication
+from PySide6.QtGui import QGuiApplication, QPalette, QColor
 from PySide6.QtWidgets import QApplication, QSizePolicy, QMainWindow, QWidget, QVBoxLayout, QSlider, QLabel
-from PySide6.Qt3DExtras import Qt3DExtras
-from PySide6.Qt3DCore import Qt3DCore
-from PySide6.QtGui import (QGuiApplication, QVector3D, QQuaternion)
-
-class Cube(Qt3DExtras.Qt3DWindow):
-    def __init__(self):
-        super().__init__()
-
-        # Camera
-        self.camera().lens().setPerspectiveProjection(45, 16 / 9, 0.1, 1000)
-        self.camera().setPosition(QVector3D(0, 0, 5))
-        self.camera().setViewCenter(QVector3D(0, 0, 0))
-
-        # For camera controls
-        self.createScene()
-
-        self.setRootEntity(self.rootEntity)
-
-    def createScene(self):
-        # Root entity
-        self.rootEntity = Qt3DCore.QEntity()
-
-        # Material
-        self.material = Qt3DExtras.QPhongMaterial(self.rootEntity)
-
-        # Cube
-        self.cubeEntity = Qt3DCore.QEntity(self.rootEntity)
-        self.cubeMesh = Qt3DExtras.QCuboidMesh()
-
-        self.cubeTransform = Qt3DCore.QTransform()
-
-        self.cubeEntity.addComponent(self.cubeMesh)
-        self.cubeEntity.addComponent(self.cubeTransform)
-        self.cubeEntity.addComponent(self.material)
-
-    def update_rotation(self, yaw: float, pitch: float, roll: float):
-        # Create the quaternion rotations for each axis
-        quaternionYaw = QQuaternion.fromEulerAngles(0, yaw, 0)
-        quaternionPitch = QQuaternion.fromEulerAngles(pitch, 0, 0)
-        quaternionRoll = QQuaternion.fromEulerAngles(0, 0, roll)
-
-        # Combine all rotations in the proper order
-        finalQuaternion = quaternionYaw * quaternionPitch * quaternionRoll
-
-        # Apply the final quaternion rotation to the cube
-        self.cubeTransform.setRotation(finalQuaternion)
-
+from PySide6.QtGui import (QGuiApplication)
+from gyroscope import Cube
 
 class MainWindow(QMainWindow):
-
     def __init__(self):
         super().__init__()
 
@@ -83,7 +31,6 @@ class MainWindow(QMainWindow):
 
         self.cube = Cube()
         self.container = QWidget.createWindowContainer(self.cube)
-        geometry = QGuiApplication.primaryScreen().geometry()
         self.container.setFixedSize(200, 200)
 
         self.container.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
